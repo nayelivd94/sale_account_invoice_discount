ee96202bae62de0c491a6be0031939dc55bc994e
Nay3li <nayeli.valencia@xmarts.com.mx>
2017-12-04 09:55:56 -0600

    adaptado a interkey

---
+++ models/sale.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Sales and Invoice Discount Management
+#    Copyright (C) 2015 BrowseInfo(<http://www.browseinfo.in>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import openerp.addons.decimal_precision as dp
+from openerp import api, fields, models, _
+
+
+class sale_order(models.Model):
+    _inherit = 'sale.order'
+
+    @api.depends('discount_amount')
+    @api.multi
+    def _calculate_discount(self):
+        res=0.0
+        discount = 0.0
+        for self_obj in self:
+            if self_obj.discount_method == 'fix':
+                discount = self_obj.discount_amount
+                res = discount
+            elif self_obj.discount_method == 'per':
+                discount = self_obj.amount_untaxed * (self_obj.discount_amount/ 100)
+                res = discount
+            else:
+                res = discount
+        return res
+
+
+    @api.depends('order_line.price_total','discount_amount')
+    def _amount_all(self):
+        """
+        Compute the total amounts of the SO.
+        """
+        cur_obj = self.env['res.currency']
+        for order in self:
+            amount_untaxed = amount_tax = 0.0
+            for line in order.order_line:
+                amount_untaxed += line.price_subtotal
+                amount_tax += line.price_tax
+            order.update({
+                          'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
+                          'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+                          'amount_total': amount_untaxed + amount_tax,
+                          })
+            res = self._calculate_discount()
+            order.update({'discount_amt' : res,
+                          'amount_total': amount_untaxed + amount_tax-res
+                          })
+
+
+    discount_method = fields.Selection([('fix', 'Fixed'), ('per', 'Percentage')], 'Discount Method')
+    discount_amount = fields.Float('Discount Amount')
+    discount_amt = fields.Monetary(compute='_amount_all', string='- Discount', digits_compute=dp.get_precision('Account'), store=True, readonly=True)
+
+
+
+
+                          
+    @api.multi
+    def _prepare_invoice(self):
+        """
+        Prepare the dict of values to create the new invoice for a sales order. This method may be
+        overridden to implement custom invoice generation (making sure to call super() to establish
+        a clean extension chain).
+        """
+        self.ensure_one()
+        journal_id = self.env['account.invoice'].default_get(['journal_id'])['journal_id']
+        if not journal_id:
+            raise UserWarning(_('Please define an accounting sale journal for this company.'))
+        invoice_vals = {
+            'name': self.client_order_ref or '',
+            'origin': self.name,
+            'type': 'out_invoice',
+            'reference': self.client_order_ref or self.name,
+            'account_id': self.partner_invoice_id.property_account_receivable_id.id,
+            'partner_id': self.partner_invoice_id.id,
+            'journal_id': journal_id,
+            'discount_method': self.discount_method,
+            'discount_amount': self.discount_amount,
+            'discount_amt': self.discount_amt,
+            'currency_id': self.pricelist_id.currency_id.id,
+            'comment': self.note,
+            'payment_term_id': self.payment_term_id.id,
+            'fiscal_position_id': self.fiscal_position_id.id or self.partner_invoice_id.property_account_position_id.id,
+            'company_id': self.company_id.id,
+            'user_id': self.user_id and self.user_id.id,
+            'team_id': self.team_id.id
+        }
+        return invoice_vals
+
+
+class sale_order_line(models.Model):
+    _inherit = 'sale.order.line'
+
+    is_apply_on_discount_amount =  fields.Boolean("Tax Apply After Discount")
+    discount_method = fields.Selection([('fix', 'Fixed'), ('per', 'Percentage')], 'Discount Method')
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:s
